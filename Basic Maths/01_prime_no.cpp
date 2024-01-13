52cffaa6e8af781dc5d1637202c490984949fdd7
Udit Panchal <panchaludit123@gmail.com>
2024-01-13 11:48:27 +0530

    Basic Maths : prime number

---
+++ Basic Maths/01_prime_no.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    if(isPrime(n))
+        cout<<"it is prime no."<<endl;
+    else  
+        cout<<"not a prime no."<<endl;
+        
+    return 0;
+}
