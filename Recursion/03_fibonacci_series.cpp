9e4a4c5bf4cecbbc26c17a43ef5a5c36ed35d04d
Udit Panchal <panchaludit123@gmail.com>
2024-01-14 23:23:22 +0530

    Recursion : Fibonacci Series

---
+++ Recursion/03_fibonacci_series.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+
+    if (n == 1)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Fibonacci series up to " << n << " terms: ";
+    for (int i = 0; i < n; ++i)
+    {
+        cout << fib(i) << " ";
+    }
+
+    return 0;
+}
