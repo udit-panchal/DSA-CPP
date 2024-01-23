b77fab8b4ab0d96eb38bdcd5cd5e120c74711d72
Udit Panchal <panchaludit123@gmail.com>
2024-01-23 15:07:32 +0530

    Linkedlist : implementation of linkedlist

---
+++ Linked List/01_linked_list.cpp
@@ -0,0 +1,249 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    // Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // Destructor
+    ~Node()
+    {
+        int value = this->data;
+
+        // Memory deallocation
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "Memory free with data : " << value << endl;
+    }
+};
+
+// INSERTION OF NODE AT HEAD , AT TAIL AND AT ANY SPECIFIC POSITION
+
+// Insert a new node at the beginning of the linked list
+void insertAtHead(Node *&head, int d)
+{
+    // Create a new node with the given data
+    Node *newNode = new Node(d);
+
+    // Set the 'next' pointer of the new node to the current head of the linked list
+    newNode->next = head;
+
+    // Update the head of the linked list to be the new node
+    head = newNode;
+}
+
+// Insert a new node at the end of the linked list
+void insertAtTail(Node *&tail, int d)
+{
+    // Create a new node with the given data
+    Node *newNode = new Node(d);
+
+    // Set the 'next' pointer of the current tail to the new node
+    tail->next = newNode;
+
+    // Update the tail to be the new node
+    tail = newNode;
+}
+
+// Insert a new node at a specific position in the linked list
+void insertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+
+    // position 1, inserting at start
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+
+    // Create a temporary pointer 'temp' and initialize it with the head of the linked list
+    Node *temp = head;
+    int count = 1;
+
+    // Go to the position where the new node will be inserted
+    while (count < position - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    // insert at last position
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    // creating new node
+    Node *newNode = new Node(d);
+
+    // Putting the new node between two nodes
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+// DELETION OF NODE AT HEAD , AT TAIL AND AT ANY SPECIFIC POSITION
+
+// Delete a node at a specific position in the linked list
+void deleteNode(int position, Node *&head, Node *&tail)
+{
+    // for deleting the first node
+    if (position == 1)
+    {
+        // create a temporary node and initialize it with the head
+        Node *temp = head;
+
+        // move head forward to the next node
+        head = head->next;
+
+        // unlink the node's next pointer
+        temp->next = NULL;
+
+        // delete the node
+        delete temp;
+    }
+    else
+    {
+        // current node pointer, initialized to the head
+        Node *curr = head;
+
+        // previous node pointer, initially set to NULL
+        Node *prev = NULL;
+
+        // counter to keep track of the position while traversing the list
+        int count = 1;
+
+        // go to the position where the deletion should occur
+        while (count < position)
+        {
+            // move prev to the current position
+            prev = curr;
+
+            // move curr to the next node
+            curr = curr->next;
+
+            // increment the counter
+            count++;
+        }
+
+        // update the tail if the node to be deleted is the last node
+        if (curr == tail) tail = prev;
+
+        // unlink the current node by updating the next pointer of the previous node
+        prev->next = curr->next;
+
+        // set the next pointer of the current node to NULL
+        curr->next == NULL;
+
+        // delete the current node
+        delete curr;
+    }
+}
+
+void deleteNodeWithValue(int value, Node *&head, Node *&tail)
+{
+    // Current node pointer, initialized to the head
+    Node *curr = head;
+
+    // Previous node pointer, initially set to NULL
+    Node *prev = NULL;
+
+    // Traverse the linked list to find the node with the specified value
+    while (curr != NULL && curr->data != value)
+    {   // Move prev to the current position
+        prev = curr;
+
+        // Move curr to the next node
+        curr = curr->next;
+    }
+
+    // If the value is not found
+    if (curr == NULL)
+    {
+        cout << "Node with value " << value << " not found." << endl;
+        return;
+    }
+
+    // Update the tail if the node to be deleted is the last node
+    if (curr == tail) tail = prev;
+
+    // Unlink the current node by updating the next pointer of the previous node
+    if (prev != NULL) prev->next = curr->next;
+
+    // If the node to be deleted is the head, update the head
+    else head = curr->next;
+
+    // Set the next pointer of the current node to NULL
+    curr->next = NULL;
+
+    // Delete the current node
+    delete curr;
+}
+
+
+// PRINTING LINKED LIST
+
+// Print the elements of the linked list
+void print(Node *&head)
+{
+    cout << "linked list : ";
+    // Create a temporary pointer 'temp' and initialize it with the head of the linked list
+    Node *temp = head;
+
+    // iterate through the linked list until the end is reached
+    while (temp != NULL)
+    {
+        // Print the data of the current node
+        cout << temp->data << " -> ";
+
+        // Move to the next node in the linked list
+        temp = temp->next;
+    }
+    cout << "NULL " << endl;
+}
+
+int main()
+{
+    // Create the first node
+    Node *node1 = new Node(2);
+    // cout << node1->data << endl;
+    // cout << node1->next << endl
+    //      << endl;
+
+    // Initialize head and tail with the first node
+    Node *head = node1;
+    Node *tail = node1;
+
+    // Insert nodes at different positions
+    insertAtHead(head, 12);
+    insertAtHead(head, 19);
+    insertAtTail(tail, 7);
+    insertAtPosition(tail, head, 2, 1111);
+
+    // Print the linked list
+    print(head);
+
+    // Delete a node at a specific position
+    deleteNode(5, head, tail);
+
+    // Print the linked list after deletion
+    print(head);
+
+    // Print the head and tail values
+    cout << "Head : " << head->data << endl;
+    cout << "Tail : " << tail->data << endl;
+
+    return 0;
+}
---
+++ Linked List/02_linked_list.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+
+    LinkedList()
+    {
+        this->head = NULL;
+    }
+
+    void insertAtBegin(int data)
+    {
+        Node *newNode = new Node(data);
+        newNode->next = head;
+
+        if (head == NULL)
+            head = tail = newNode;
+
+        else
+            head = newNode;
+    }
+
+    void insertAtTail(int data)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+            head = tail = newNode;
+
+        else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    void insertAtPosition(int data, int position)
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+
+        if (position == 1)
+        {
+            insertAtBegin(data);
+            return;
+        }
+
+        int count = 1;
+
+        while (count != position)
+        {
+            prev = curr;
+            curr = curr->next;
+            count++;
+        }
+
+        Node *newNode = new Node(data);
+        prev->next = newNode;
+        newNode->next = curr;
+    }
+
+    void replaceValue(int newData, int oldValue)
+    {
+
+        Node *temp = head;
+        int count = 1;
+        while (temp->data != oldValue)
+        {
+            temp = temp->next;
+            count++;
+        }
+        if(temp != NULL)
+            temp->data = newData;
+        else
+            cout<<"Not Found. "<<endl;
+    }
+
+    void deleteNode(int position)
+    {
+        if(position == 1)
+        {
+            Node* temp = head;
+            head = head -> next;
+            temp->next = NULL;
+            delete temp;
+        }
+        else{
+            Node* curr = head;
+            Node* prev = NULL;
+            int count = 1;
+
+
+            while(count != position)
+            {
+                prev = curr;
+                curr = curr->next;
+                count++;
+            }
+
+            if(curr == tail) tail = prev;
+
+            prev->next = curr->next;
+            curr->next = NULL;
+            delete curr;
+        }
+    }
+
+    void print()
+    {
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << endl;
+
+        cout << "Head :" << head->data << endl;
+        cout << "Tail :" << tail->data << endl;
+    }
+};
+
+int main()
+{
+
+    LinkedList list;
+
+    list.insertAtBegin(1);
+    list.insertAtBegin(2);
+    list.insertAtBegin(3);
+    list.insertAtBegin(4);
+
+    list.insertAtTail(0);
+    list.insertAtTail(9);
+    list.insertAtTail(8);
+    list.insertAtTail(7);
+
+    list.insertAtPosition(123, 2);
+
+    list.replaceValue(456, 123);
+
+    list.print();
+
+    return 0;
+}
