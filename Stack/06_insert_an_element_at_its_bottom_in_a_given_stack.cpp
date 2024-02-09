89360ec4359f2cf7d59187fe6a09582ee7a57efc
Udit Panchal <panchaludit123@gmail.com>
2024-02-09 15:25:08 +0530

    Stack : Insert an element at its bottom of the stack

---
+++ Stack/06_insert_an_element_at_its_bottom_in_a_given_stack.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(stack<int> &inputStack, int element)
+{
+    if (!inputStack.empty())
+    {
+        inputStack.push(element);
+        return;
+    }
+
+    int temp = inputStack.top();
+    inputStack.pop();
+
+    solve(inputStack, element);
+
+    inputStack.push(temp);
+}
+
+void insertBottom(stack<int> &inputStack, int element)
+{
+    solve(inputStack, element);
+}
+
+void print(stack<int> str)
+{
+    cout << "Elements in the stack are : ";
+    while (!str.empty())
+    {
+        cout << str.top() << " ";
+        str.pop();
+    }
+    cout << endl;
+}
+
+int main()
+{
+    stack<int> inputStack;
+    inputStack.push(111);
+    inputStack.push(222);
+    inputStack.push(333);
+    inputStack.push(444);
+    inputStack.push(555);
+
+    cout << " 1 :-> ";
+    print(inputStack);
+
+    insertBottom(inputStack, 999);
+
+    cout << " 2 :-> ";
+    print(inputStack);
+
+    return 0;
+}
