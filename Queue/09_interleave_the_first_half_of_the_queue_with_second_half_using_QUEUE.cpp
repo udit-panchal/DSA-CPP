d4ec848f8da26c5ede016a1163cdddb51bee2ad1
Udit Panchal <panchaludit123@gmail.com>
2024-02-15 12:44:18 +0530

    Queue : Interleave the first half of the queue with second half using QUEUE

---
+++ Queue/09_interleave_the_first_half_of_the_queue_with_second_half_using_QUEUE.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void interleave_two_half_of_queue(queue<int> &q, int n)
+{
+    queue<int> second_Half;
+    int mid = n / 2;
+
+    for (int i = 0; i < mid; i++)
+    {
+        second_Half.push(q.front());
+        q.pop();
+    }
+
+    while (!second_Half.empty())
+    {
+        q.push(second_Half.front());
+        second_Half.pop();
+        q.push(q.front());
+        q.pop();
+    }
+}
+
+int main()
+{
+
+    queue<int> q;
+
+    q.push(11);
+    q.push(12);
+    q.push(13);
+    q.push(14);
+    q.push(15);
+    q.push(16);
+    q.push(17);
+    q.push(18);
+    q.push(19);
+    q.push(20);
+
+    int n = q.size();
+    interleave_two_half_of_queue(q, n);
+
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
+
+/*
+    # Interleave the first half of the queue with second half using QUEUE
+
+    Given a queue of integers of even length, rearrange the elements by interleaving the first half of the queue with the second half of the queue.
+
+    Input :  1 2 3 4
+    Output : 1 3 2 4
+
+    Input : 11 12 13 14 15 16 17 18 19 20
+    Output : 11 16 12 17 13 18 14 19 15 20
+*/
