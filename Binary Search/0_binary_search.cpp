273c0cc6af73dface2cf69b306c60abad72d6d72
Udit Panchal <panchaludit123@gmail.com>
2024-02-17 16:14:44 +0530

    Binary Search : Binary Search

---
+++ Binary Search/0_binary_search.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        if (arr[mid] > key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+        mid = (start + end) / 2;
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 14, 16};
+
+    // cout<<binarySearch(even, 6, 12)<<endl;
+    int ans = binarySearch(even, 6, 18);
+    cout << "Found on index : " << ans;
+}
