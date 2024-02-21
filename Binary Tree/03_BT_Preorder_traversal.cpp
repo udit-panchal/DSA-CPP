89cee451e08a88ddf4410d1ad4b04d17b55d41d8
Udit Panchal <panchaludit123@gmail.com>
2024-02-21 17:59:28 +0530

    Binary Tree : Pre-Order Traversal

--- Binary Tree/03_BT_Preorder_traversal.cpp
+++ Binary Tree/03_BT_Preorder_traversal.cpp
@@ -44,7 +44,7 @@ int main()
 
     bt.root = new Node(1);
 
-    bt.root->left = new Node(2);
+    bt.root->left = new Node(2); 
     bt.root->right = new Node(3);
 
     bt.root->left->left = new Node(4);
