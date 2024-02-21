871e29932e1e5467fd90741c59f6808a242f5e8a
Udit Panchal <panchaludit123@gmail.com>
2024-02-21 17:35:54 +0530

    Binary Search : In-Order Traversal

---
+++ Binary Tree/04_BT_Inorder_traversal.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class BinaryTree
+{
+public:
+    Node *root;
+
+    BinaryTree()
+    {
+        root = nullptr;
+    }
+
+    void inorder(Node *root)
+    {
+        if (root == nullptr)
+            return;
+
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+};
+
+int main()
+{
+
+    BinaryTree bt;
+
+    bt.root = new Node(1);
+
+    bt.root->left = new Node(2);
+    bt.root->right = new Node(3);
+
+    bt.root->left->left = new Node(4);
+    bt.root->left->left->left = new Node(23);
+    bt.root->left->right = new Node(5);
+    bt.root->left->right->right = new Node(45);
+    bt.root->left->right->right->right = new Node(78);
+    bt.root->left->right->right->right->right = new Node(67);
+    bt.root->right->left = new Node(6);
+    bt.root->right->right = new Node(7);
+
+    ///////////////////////////////////
+    //                               //
+    //                               //
+    //               1               //
+    //             /   \             //
+    //           2       3           //
+    //          / \     / \          //
+    //         4   5   6   7         //
+    //        /     \                //
+    //       23      45              //
+    //                \              //
+    //                 78            //
+    //                  \            //
+    //                   67          //
+    //                               //
+    //                               //
+    ///////////////////////////////////
+
+    bt.inorder(bt.root);
+    return 0;
+}
